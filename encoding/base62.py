8e1c37a55455df517edaa24739c5092a0c83c885
luutanhung <luutanhung.dev@gmail.com>
2025-08-31 08:23:12 +0700

    feat: add base62 number decoding

--- encoding/base62.py
+++ encoding/base62.py
@@ -1,8 +1,9 @@
 def base62_encode(num: str) -> str:
     """
     >>> base62_encode(1234567890)
-    1ly7vk
+    '1ly7vk'
     """
+
     import string
 
     chars: list[str] = string.digits + string.ascii_letters
@@ -14,6 +15,22 @@ def base62_encode(num: str) -> str:
         num, remainder = divmod(num, 62)
         base62.append(chars[remainder])
     return "".join(reversed(base62))
+
+
+def base62_decode(s: str) -> int:
+    """
+    >>> base62_decode("1ly7vk")
+    1234567890
+    """
+
+    import string
+
+    chars: list[str] = string.digits + string.ascii_letters
+    char_mp = {chr: i for i, chr in enumerate(chars)}
+    num: int = 0
+    for chr in s:
+        num = num * 62 + char_mp[chr]
+    return num
 
 
 if __name__ == "__main__":
